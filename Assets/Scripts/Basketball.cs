80d7bdc616bfe8c649b3945dc181b393c0db5f2e
Komarov <kkmrvvv@gmail.com>
2024-05-25 11:58:43 +0300

    v2

--- Assets/Scripts/Basketball.cs
+++ Assets/Scripts/Basketball.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Basketball : MonoBehaviour
 {
@@ -15,10 +17,51 @@ public class Basketball : MonoBehaviour
     private bool _isBallFlying = false;
     private float _t = 0;
 
+    private Vector3 randomVector;
+
     // Update is called once per frame
+    private void Start()
+    {
+        randomVector = new Vector3(Random.Range(0, 3), Random.Range(0, 2), Random.Range(0, 2));
+
+    }
+
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = new Vector3(0, 0, 0);
+        bool isGetKey = false;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction = new Vector3(0, 0, 1);
+            isGetKey = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = new Vector3(0, 0, -1);
+            isGetKey = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = new Vector3(1, 0, 0);
+            isGetKey = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = new Vector3(-1, 0, 0);
+            isGetKey = true;
+        }
+
+        if (isGetKey)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+
         transform.position += direction * _speed * Time.deltaTime;
         transform.LookAt(transform.position + direction);
 
@@ -29,7 +72,7 @@ public class Basketball : MonoBehaviour
                 _ball.position = _posOverhead.position;
                 _arms.localEulerAngles = Vector3.right * -25; // 180
 
-                transform.LookAt(_target.position);
+                //transform.LookAt(_target.position);
             }
             else
             {
@@ -52,7 +95,8 @@ public class Basketball : MonoBehaviour
             float t01 = _t / duration;
 
             Vector3 a = _posOverhead.position;
-            Vector3 b = _target.position;
+            //Vector3 b = _target.position;
+            Vector3 b = _target.position + randomVector;
             Vector3 pos = Vector3.Lerp(a, b, t01);
 
             Vector3 arc = Vector3.up * 5 * Mathf.Sin(t01 * 3.14f);
@@ -69,10 +113,15 @@ public class Basketball : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!_isBallInHands && !_isBallFlying)
+        if (other.tag == "Ball")
         {
-            _isBallInHands = true;
-            _ball.GetComponent<Rigidbody>().isKinematic = true;
+            if (!_isBallInHands && !_isBallFlying)
+            {
+                randomVector = new Vector3(Random.Range(0, 3), Random.Range(0, 2), Random.Range(0, 2));
+                _isBallInHands = true;
+                _ball.GetComponent<Rigidbody>().isKinematic = true;
+                Debug.Log("Balllllll");
+            }
         }
     }
 }
